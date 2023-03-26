47ac927ebbe887d4708ab5da6e3f55345dbe3656
Arshadaa <mdarshadali446@gmail.com>
2023-03-27 03:47:16 +0530

    Spring-Batch-Application

--- src/test/java/com/arshad/SpringBatchApplication.java
+++ src/test/java/com/arshad/SpringBatchApplication.java
@@ -1,4 +1,4 @@
-package com.techprimers.springbatchexample1;
+package com.arshad;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringBatchExample1ApplicationTests {
+public class SpringBatchApplication {
 
 	@Test
 	public void contextLoads() {
